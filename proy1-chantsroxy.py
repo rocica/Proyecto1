bf25b93b40dc7fd9f7367dbe84ad3d6277f5e4a6
chantaldg <105188742+chantaldg@users.noreply.github.com>
2023-04-24 13:36:16 -0500

    comments proy1

--- proy1-chantsroxy.py
+++ proy1-chantsroxy.py
@@ -3,27 +3,22 @@ import tkinter as tk
 
 
 def dibujar_cuadrado():
-    # Definir los vértices del cuadrado
-    vertices = [(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)]
+    vertices = [(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)] #Se defininen los vértices del cuadrado
 
-    # Extraer las coordenadas x e y de los vértices
+    #Extraer las coordenadas x & y de los vértices  
     xs = [vertice[0] for vertice in vertices]
     ys = [vertice[1] for vertice in vertices]
 
-    # Graficar el cuadrado
-    plt.plot(xs, ys)
+    plt.plot(xs, ys) # Se grafica el cuadrado usando la función plot
 
 
 def dibujar_triangulo():
-    # Definir los vértices del triángulo
-    vertices = [(0, 0), (1, 0), (0.5, 0.5), (0, 0)]
+    vertices = [(0, 0), (1, 0), (0.5, 0.5), (0, 0)]     # Definir los vértices del triángulo
 
-    # Extraer las coordenadas x e y de los vértices
     xs = [vertice[0] for vertice in vertices]
     ys = [vertice[1] for vertice in vertices]
 
-    # Graficar el triángulo
-    plt.plot(xs, ys)
+    plt.plot(xs, ys) # Se grafica el cuadrado usando la función plot
 
 
 def dibujar_figura(figura):
@@ -32,30 +27,20 @@ def dibujar_figura(figura):
     elif figura == "Triángulo":
         dibujar_triangulo()
 
-    # Establecer la relación de aspecto en 'igual' para asegurar una forma cuadrada
-    plt.axis('equal')
-    # Mostrar la gráfica
-    plt.show()
+    plt.axis('equal') #Función que establece los límites de los ejes de un gráfico en igual escala
+    plt.show() #Enseña el gráfico
 
 
-# Crear la ventana de tkinter
+#TKINTER
 root = tk.Tk()
 root.title("Dibujante de Figuras")
-
-# Establecer el color de fondo en rosa claro
 root.configure(bg="#FFD9EB")
-
-# Crear una etiqueta para la selección de figura
 etiqueta_figura = tk.Label(root, text="Seleccionar Figura:")
 etiqueta_figura.pack()
-
-# Crear botones para la selección de figura con color fucsia
 boton_cuadrado = tk.Button(
     root, text="Cuadrado", command=lambda: dibujar_figura("Cuadrado"), fg="fuchsia")
 boton_cuadrado.pack()
 boton_triangulo = tk.Button(
     root, text="Triángulo", command=lambda: dibujar_figura("Triángulo"), fg="fuchsia")
 boton_triangulo.pack()
-
-# Iniciar el tkinter
 root.mainloop()
